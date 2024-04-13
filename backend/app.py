0492a148e4100f3ba89b7a42df847eb06909663f
Seunggyun <hsg8234@naver.com>
2024-04-13 19:27:54 +0900

    set cors initially

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,23 @@ import pandas as pd
 
 from flask import Flask
 from flask import render_template, request, jsonify
+from flask_cors import CORS
 
 import joblib
 
 # create an app and load the model
 app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": ["http://localhost", "https://teamgigo.netlify.app"]}})
+
 ## model = joblib.load('model.pkl')
 
-@app.route('/predict', method=['GET'])
+@app.route('/predict', methods=['GET'])
 def predict():
     try:
         # get the query parameters
-        data = request.get_json()
-        df_input = pd.DataFrame(data, index=[0])
-        ## prediction = model.predict(df_input)
+        # data = request.get_json()
+        # df_input = pd.DataFrame(data, index=[0])
+        # ## prediction = model.predict(df_input)
         prediction = [1]
         return jsonify({'prediction': prediction[0]})
     except Exception as e:
