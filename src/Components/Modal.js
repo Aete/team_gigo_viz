0b448ffef4338a8cda70a0016ec247cb1fe321e9
Seunggyun <hsg8234@naver.com>
2025-01-24 19:49:34 +0900

    upgrade the modal design

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -73,6 +73,24 @@ const ModalBody = styled.div`
   & a {
     text-decoration: underline;
   }
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
 `;
 
 export default function Modal({ handleModal }) {
